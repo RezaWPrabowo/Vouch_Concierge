592fd18f9d21ae68d0b2973d49644b437ec260ea
Reza <reza@atto.id>
2022-02-11 21:58:24 +0700

    Heroku last config

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-var express = require('express');
-var serveStatic = require('serve-static');
+const express = require('express');
+const serveStatic = require("serve-static")
+const path = require('path');
 
 app = express();
 
-app.use(serveStatic(__dirname + "/dist"));
+app.use(serveStatic(path.join(__dirname, 'dist')));
 
-var port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.listen(port);
-
-console.log('server started '+ port);
